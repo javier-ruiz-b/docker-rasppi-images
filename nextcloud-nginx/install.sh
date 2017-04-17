2693c6138cd4bd3d2d320185ccbc459ad5fbb0de
Javier Ruiz <private@email.com>
2017-04-17 13:00:39 +0200

    Bugfix: dockerfile fixes

--- nextcloud-nginx/install.sh
+++ nextcloud-nginx/install.sh
@@ -8,5 +8,5 @@ mysql -u root -p$DBPASS -e "CREATE DATABASE nextcloud;"
 mysql -u root -p$DBPASS -e "GRANT ALL ON nextcloud.* to 'nextcloud'@'localhost' IDENTIFIED BY '$DBPASS';"
 mysql -u root -p$DBPASS -e "FLUSH PRIVILEGES;"
 
-sed -i "20i  \'memcache.local\' => \'\\\OC\\\Memcache\\\APCu\'," config.php
+#sed -i "20i  \'memcache.local\' => \'\\\OC\\\Memcache\\\APCu\'," config.php
 
--- nextcloud-nginx/run.sh
+++ nextcloud-nginx/run.sh
@@ -9,4 +9,4 @@ fi
 mkdir -p config data db
 docker build --build-arg DBPASS=$1 . -t nextcloud-nginx
 
-docker run -p80:80 -p443:443 --rm --name nextcloudnginx nextcloud-nginx
+docker run -p80:80 -p443:443 -it --rm --name nextcloudnginx nextcloud-nginx
