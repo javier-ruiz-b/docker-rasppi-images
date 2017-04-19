415a1ad39dbc492242646f0bf1c91d31ef6e5e60
Javier Ruiz <private@email.com>
2017-04-19 14:01:30 +0200

    Feature: working nextcloud nginx docker container

--- nextcloud-nginx/install.sh
+++ nextcloud-nginx/install.sh
@@ -8,5 +8,43 @@ mysql -u root -p$DBPASS -e "CREATE DATABASE nextcloud;"
 mysql -u root -p$DBPASS -e "GRANT ALL ON nextcloud.* to 'nextcloud'@'localhost' IDENTIFIED BY '$DBPASS';"
 mysql -u root -p$DBPASS -e "FLUSH PRIVILEGES;"
 
+# ///////////////// nextcloud configuration script ///////////////////
+ocpath='/var/www/nextcloud'
+htuser='www-data'
+htgroup='www-data'
+rootuser='root'
+
+printf "Creating possible missing Directories\n"
+mkdir -p $ocpath/data
+mkdir -p $ocpath/assets
+mkdir -p $ocpath/updater
+
+printf "chmod Files and Directories\n"
+find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
+find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
+chmod 755 ${ocpath}
+
+printf "chown Directories\n"
+chown -R ${rootuser}:${htgroup} ${ocpath}/
+chown -R ${htuser}:${htgroup} ${ocpath}/apps/
+chown -R ${htuser}:${htgroup} ${ocpath}/assets/
+chown -R ${htuser}:${htgroup} ${ocpath}/config/
+chown -R ${htuser}:${htgroup} ${ocpath}/data/
+chown -R ${htuser}:${htgroup} ${ocpath}/themes/
+chown -R ${htuser}:${htgroup} ${ocpath}/updater/
+
+chmod +x ${ocpath}/occ
+
+printf "chmod/chown .htaccess\n"
+if [ -f ${ocpath}/.htaccess ]
+ then
+  chmod 0644 ${ocpath}/.htaccess
+  chown ${rootuser}:${htgroup} ${ocpath}/.htaccess
+fi
+if [ -f ${ocpath}/data/.htaccess ]
+ then
+  chmod 0644 ${ocpath}/data/.htaccess
+  chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
+fi
 #sed -i "20i  \'memcache.local\' => \'\\\OC\\\Memcache\\\APCu\'," config.php
 
---
+++ nextcloud-nginx/nextcloud-config.php
@@ -0,0 +1,15 @@
+<?php
+$CONFIG = array (
+  'trusted_domains' =>
+  array (
+    0 => '127.0.0.1',
+  ),
+  'datadirectory' => '/var/www/nextcloud/data',
+  'overwrite.cli.url' => 'https://127.0.0.1',
+  'dbtype' => 'mysql',
+  'version' => '11.0.2.7',
+  'memcache.local' => '\OC\Memcache\APCu',
+  'logtimezone' => 'UTC',
+  'installed' => false,
+);
+
