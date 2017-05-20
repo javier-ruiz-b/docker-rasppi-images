a3ed0dc977423f25dbd1c4c8b99e06f8a80606f4
Javier Ruiz <private@email.com>
2017-05-20 13:41:23 +0200

    Added MariaDB volume

--- owncloud-nginx/build-and-run.sh
+++ owncloud-nginx/build-and-run.sh
@@ -9,8 +9,13 @@ set -e
 set -x
 
 docker image build --build-arg DBPASS=$1 . -t owncloud-nginx
+docker volume create owncloud-db
 docker container run -p81:80 -p444:443 \
 	-v /etc/letsencrypt:/etc/letsencrypt \
 	-v /nextcloud-data:/nextcloud-data \
+	-v owncloud-db:/var/lib/mysql
 	--name owncloud \
-	owncloud-nginx
+	owncloud-nginx \
+	bash -x /root/install.sh $1
+
+docker container start owncloud
