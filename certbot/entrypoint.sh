e6aed3fa41745999e0447a75dbac2ecece04a747
Treso <private@email.com>
2017-07-02 20:50:13 +0200

    Bugfix: corrected certbot update

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -3,9 +3,6 @@ set -x
 set -e
 
 #let's encrypt 
-echo "non-interactive = True" > /etc/letsencrypt/cli.ini
-echo "email = tras3@gmx.net" >> /etc/letsencrypt/cli.ini
 #certbot --apache -d treso.info -d www.treso.info --agree-tos
-
 certbot renew
 
