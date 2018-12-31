d3ceec8b94432af5b051caf6c59de80992ca80b1
Javier Ruiz <javier.ruiz@outlook.de>
2018-12-31 13:21:32 +0100

    Updated settings

--- nextcloud-official/start.sh
+++ nextcloud-official/start.sh
@@ -15,5 +15,11 @@ MYSQL_USER=nextcloud
 NEXTCLOUD_ADMIN_PASSWORD=$(cat /etc/password)
 EOL
 
+
+sysctl -w net.core.somaxconn=1024
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+sysctl vm.overcommit_memory=1
+echo 512 > /proc/sys/net/core/somaxconn
+
 docker-compose build --pull
 docker-compose up
