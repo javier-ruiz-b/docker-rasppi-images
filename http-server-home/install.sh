0506a6c253d6b5eb8071161286eaf4fbee9fcd4c
Javier Ruiz <triomphe3@gmail.com>
2020-11-29 12:25:40 +0000

    Bugfix: fix errors

--- http-server-home/install.sh
+++ http-server-home/install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-set -euo pipefail
+set -euxo pipefail
 
 if [ "$(id -u)" != "0" ]; then
     echo "Run as root"
     exit 1
 fi
 
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
 
 dir="$(pwd)"
 cp -f http-server-home.service /etc/systemd/system
 chmod +x main.py
 
-sed -i "s/{pythonfile}/$dir\/main.py/g" /etc/systemd/system/http-server-home.service
+sed -i "s#{pythonfile}#$dir/main.py#g" /etc/systemd/system/http-server-home.service
 
 systemctl daemon-reload
 systemctl enable http-server-home.service
--- http-server-home/main.py
+++ http-server-home/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 from flask import Flask, json, request
 import os
 
@@ -8,18 +8,19 @@ companies = [{"id": 1, "name": "Company One"},
 api = Flask(__name__)
 
 
-def set_relay(device, value):
+def set_relay(device, setValue):
     value = 0
+    print(f"pilight-control -d {device} -s {setValue}")
     for _ in range(3):
-        value = os.system(f"pilight-control -d {device} -s {value}")
-    return value
+        value = os.system(f"pilight-control -d {device} -s {setValue}")
+    return "ok"
 
 @api.route('/all-off', methods=['GET'])
 def all_off():
     value = 0
     for device in ['intertechno0', 'intertechno1', 'intertechno2']:
         value = set_relay(device, "off")
-    return value
+    return "ok"
 
 @api.route('/light-control', methods=['GET'])
 def light():
@@ -32,4 +33,4 @@ def light():
 
 
 if __name__ == '__main__':
-    api.run(port=8055)  # host='0.0.0.0',
+    api.run(host='0.0.0.0', port=8055)  # host='0.0.0.0',
