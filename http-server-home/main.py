eb8b8a958e2b0a28ede61bff6d6a526d568d4bba
Javier R <javier.ruiz@outlook.de>
2020-11-29 13:28:09 +0100

    Feature: add all on

--- http-server-home/main.py
+++ http-server-home/main.py
@@ -20,6 +20,15 @@ def all_off():
     value = 0
     for device in ['intertechno0', 'intertechno1', 'intertechno2']:
         value = set_relay(device, "off")
+    os.system(f"curl http://127.0.0.1:8033/OFF")
+    return "ok"
+
+@api.route('/all-on', methods=['GET'])
+def all_on():
+    value = 0
+    for device in ['intertechno0', 'intertechno1', 'intertechno2']:
+        value = set_relay(device, "on")
+    os.system(f"curl http://127.0.0.1:8033/ON")
     return "ok"
 
 @api.route('/light-control', methods=['GET'])
