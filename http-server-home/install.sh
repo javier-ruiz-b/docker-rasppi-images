021d1975b638edc485a014ff3d390526e748cd62
Javier R <javier.ruiz@outlook.de>
2020-11-29 12:45:13 +0100

    Feature: add working http server home

---
+++ http-server-home/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ "$(id -u)" != "0" ]; then
+    echo "Run as root"
+    exit 1
+fi
+
+pip3 install -r requirements.txt
+
+dir="$(pwd)"
+cp -f http-server-home.service /etc/systemd/system
+chmod +x main.py
+
+sed -i "s/{pythonfile}/$dir\/main.py/g" /etc/systemd/system/http-server-home.service
+
+systemctl daemon-reload
+systemctl enable http-server-home.service
+systemctl restart http-server-home.service
--- http-server-home/main.py
+++ http-server-home/main.py
@@ -1,1 +1,35 @@
-# env python
+#!/usr/bin/env python
+from flask import Flask, json, request
+import os
+
+companies = [{"id": 1, "name": "Company One"},
+             {"id": 2, "name": "Company Two"}]
+
+api = Flask(__name__)
+
+
+def set_relay(device, value):
+    value = 0
+    for _ in range(3):
+        value = os.system(f"pilight-control -d {device} -s {value}")
+    return value
+
+@api.route('/all-off', methods=['GET'])
+def all_off():
+    value = 0
+    for device in ['intertechno0', 'intertechno1', 'intertechno2']:
+        value = set_relay(device, "off")
+    return value
+
+@api.route('/light-control', methods=['GET'])
+def light():
+    if request.args:
+        items = dict(request.args.items())
+        device = items['device']
+        setValue = items['set']
+        return set_relay(device, setValue)
+    return f"Missing device and set args"
+
+
+if __name__ == '__main__':
+    api.run(port=8055)  # host='0.0.0.0',
