b957fa56b05cc22162065aac86bd0c0f982b5743
Javier Ruiz <triomphe3@gmail.com>
2020-12-01 20:06:43 +0000

    Add more commands and use pilight-send

--- http-server-home/main.py
+++ http-server-home/main.py
@@ -7,37 +7,45 @@ companies = [{"id": 1, "name": "Company One"},
 
 api = Flask(__name__)
 
+lights  = { "pc" : "0",
+            "side" : "1",
+            "tv" : "2" }
 
-def set_relay(device, setValue):
-    value = 0
-    print(f"pilight-control -d {device} -s {setValue}")
-    # for _ in range(3):
-    value = os.system(f"pilight-control -d {device} -s {setValue}")
-    return "ok"
+lights_command = "pilight-send -p kaku_switch -i 13965290 "
+
+def set_light(device, value):
+    if value != "on" and value != "off":
+        return "Possible values: on or off\n"
+    return execute(lights_command + f"--unit {device} --{value}")
+
+def execute(command):
+    if os.system(command) != 0:
+        return "Error executing " + command
+    return "ok\n"
 
 @api.route('/all-off', methods=['GET'])
 def all_off():
-    value = 0
-    for device in ['intertechno0', 'intertechno1', 'intertechno2']:
-        value = set_relay(device, "off")
-    os.system(f"curl http://127.0.0.1:8033/OFF")
-    return "ok"
+    return execute(lights_command + "--off --all; curl http://127.0.0.1:8033/OFF")
+
+@api.route('/bar-on', methods=['GET'])
+def bar_on():
+    return execute("curl http://127.0.0.1:8033/ON")
 
 @api.route('/all-on', methods=['GET'])
 def all_on():
-    value = 0
-    for device in ['intertechno0', 'intertechno1', 'intertechno2']:
-        value = set_relay(device, "on")
-    os.system(f"curl http://127.0.0.1:8033/ON")
-    return "ok"
+    return execute(lights_command + "--on --all")
 
-@api.route('/light-control', methods=['GET'])
+@api.route('/tv', methods=['GET'])
+def tv():
+    return execute(lights_command + "--off --all; "+ lights_command + "--on --unit " + lights["tv"])
+
+@api.route('/light', methods=['GET'])
 def light():
     if request.args:
         items = dict(request.args.items())
-        device = items['device']
+        device = lights[items['device']]
         setValue = items['set']
-        return set_relay(device, setValue)
+        return set_light(device, setValue)
     return f"Missing device and set args"
 
 
